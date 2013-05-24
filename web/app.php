f5892794f4288894df9c4b368a31260e0754f187
Lukas Kahwe Smith <smith@pooteeweet.org>
2013-05-24 14:29:10 +0200

    remove bootstrap.cache.php

--- web/app.php
+++ web/app.php
@@ -3,12 +3,12 @@
 use Symfony\Component\ClassLoader\ApcClassLoader;
 use Symfony\Component\HttpFoundation\Request;
 
+$loader = require_once __DIR__.'/../app/autoload.php';
 
 require_once __DIR__.'/../app/AppKernel.php';
 //require_once __DIR__.'/../app/AppCache.php';
 
 $kernel = new AppKernel('prod', false);
-$kernel->loadClassCache();
 //$kernel = new AppCache($kernel);
 Request::enableHttpMethodParameterOverride();
 $request = Request::createFromGlobals();
