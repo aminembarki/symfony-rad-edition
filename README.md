81671f2bc2f2d6003d684c7c2aa074a3dfc0ddd9
Colin Frei <colin.frei@liip.ch>
2013-06-04 10:51:40 +0200

    Document Capifony deployment

--- README.md
+++ README.md
@@ -4,3 +4,27 @@ Symfony Liip RAD Edition
 This is the Symfony RAD (Rapid Application Development) Edition by [Liip](http://www.liip.ch).
 It's goal is to have an optimized setup including all the bundles we use regularly and making easy to add less-used but
 recurring functionality.
+
+
+Deployment with Capifony
+------------------------
+
+[Capifony](http://capifony.org/) is a Symfony-flavored extension of Capistrano, an automated deployment tool.
+To use Capifony, you'll first need to install it through RubyGems, by running:
+    gem install capifony
+
+Adjust the `app/config/deploy.rb` file to match your server settings. The most common configuration options are
+commented in the deploy.rb File, more extensive documentation is available for [Capistrano-](http://capifony.org/reference/capistrano.html)
+and [Symfony-specific](http://capifony.org/reference/symfony.html) config options.
+
+Next, initialize capifony on the server by running
+    cap deploy:setup
+
+Set up the parameters.yml file on the server by manually connecting to the server and adding a parameters.yml
+ file in the `[project root]/shared/app/config/` folder. Copy the content of your `app/config/parameters.yml.dist` file
+ and adjust it as necessary. You'll always want to adjust at least the `secret` variable.
+
+To actually deploy a version, run
+    cap deploy
+
+For more help on using Capifony see the [Capifony site](http://capifony.org/).
---
+++ app/config/deploy.rb
@@ -0,0 +1,75 @@
+#########################
+# Adjust the following settings to fit your server/application
+# You MUST adjust the following four settings, as they're specific to your server setup.
+
+#
+# Set a name for your applicaton
+set :application,       "set your application name here"
+
+#
+# Set the domain your application will run on.
+# The SSH connection will happen through this domain (ssh example.com)
+set :domain,            "example.com"
+
+#
+# Set the absolute path the application should be deployed to on your server
+set :deploy_to,         "/var/www/your-site"
+
+#
+# Set the URL to your git repository.
+# Your server will need to be able to check out from here.
+set :repository,        "https://github.com/liip/symfony-rad-edition.git"
+
+#
+# The settings above MUST be adjusted as they're specific to your project.
+#
+#########################
+#
+# The settings below can be adjusted to your server setup.
+# You may need to uncomment the setting first.
+#
+
+
+#
+# Don't use sudo when deploying
+# set :use_sudo, false
+
+#
+# Use a different user to connect via SSH
+# If this is commented then your current user will be used
+# set :user, "deployer"
+
+
+#
+# Set the ORM system you're using
+# Allowed are `doctrine` or `propel`
+set :model_manager,     "doctrine"
+
+#
+# Set files that should be shared between releases
+set :shared_files,      ["app/config/parameters.yml"]
+
+#
+# Get more verbose output - helpful when debugging
+# logger.level = Logger::MAX_LEVEL
+
+
+#
+#
+#########################
+#
+# The settings below usually don't need to be changed
+#
+
+
+set :use_composer,      true
+set :app_path,          "app"
+
+role :web,              domain                         # Your HTTP server, Apache/etc
+role :app,              domain                         # This may be the same as your `Web` server
+role :db,               domain, :primary => true       # This is where Symfony2 migrations will run
+
+set  :keep_releases,    3
+
+set :scm,               :git
+# Or `accurev`, `bzr`, `cvs`, `darcs`, `subversion`, `mercurial`, `perforce`, or `none`
