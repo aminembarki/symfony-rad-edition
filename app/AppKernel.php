df938ac2d7b81bdc35107d55d4db72ee710585ce
Colin Frei <colin.frei@liip.ch>
2013-06-26 15:53:23 +0200

    Added FOS Rest Bundle, JMS Serializer Bundle, and NelmioApiDocBundle

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -17,6 +17,11 @@ class AppKernel extends Kernel
             new Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle(),
             new Misd\GuzzleBundle\MisdGuzzleBundle(),
             new Doctrine\Bundle\MigrationsBundle\DoctrineMigrationsBundle(),
+
+            # REST feature branch bundles
+            new FOS\RestBundle\FOSRestBundle(),
+            new JMS\SerializerBundle\JMSSerializerBundle(),
+            new Nelmio\ApiDocBundle\NelmioApiDocBundle(),
         );
 
         if (in_array($this->getEnvironment(), array('dev', 'test'))) {
